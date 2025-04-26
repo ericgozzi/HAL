1f0297cceecc1f252ff58c3a7f0fa44c3296194c
ericgozzi <eric.gozzi.mail@gmail.com>
2025-04-26 07:37:02 +0200

    upload mathematica

--- HAL/pixels/__init__.py
+++ HAL/pixels/__init__.py
@@ -7,5 +7,102 @@ from .video import *
 
 
 # Build all color constants
-Color.constants()
+#: Pure red color (RGB: 255, 0, 0)
+RED = Color(255, 0, 0)
+
+#: Pure green color (RGB: 0, 255, 0)
+GREEN = Color(0, 255, 0)
+
+#: Pure blue color (RGB: 0, 0, 255)
+BLUE = Color(0, 0, 255)
+
+#: Yellow color, a mix of red and green (RGB: 255, 255, 0)
+YELLOW = Color(255, 255, 0)
+
+#: Cyan color, a mix of green and blue (RGB: 0, 255, 255)
+CYAN = Color(0, 255, 255)
+
+#: Magenta color, a mix of red and blue (RGB: 255, 0, 255)
+MAGENTA = Color(255, 0, 255)
+
+#: Pure white color (RGB: 255, 255, 255)
+WHITE = Color(255, 255, 255)
+
+#: Very light gray, slightly darker than white (RGB: 192, 192, 192)
+VERY_LIGHT_GRAY = Color(192, 192, 192)
+
+#: Light gray color (RGB: 192, 192, 192)
+LIGHT_GRAY = Color(192, 192, 192)
+
+#: Moderate light gray, between light gray and white (RGB: 224, 224, 224)
+MODERATE_LIGHT_GRAY = Color(224, 224, 224)
+
+#: Neutral gray, midway between black and white (RGB: 128, 128, 128)
+GRAY = Color(128, 128, 128)
+
+#: Moderate dark gray, slightly darker than neutral gray (RGB: 96, 96, 96)
+MODERATE_DARK_GRAY = Color(96, 96, 96)
+
+#: Dark gray, significantly darker than neutral gray (RGB: 80, 80, 80)
+DARK_GRAY = Color(80, 80, 80)
+
+#: Very dark gray, close to black but not completely black (RGB: 64, 64, 64)
+VERY_DARK_GRAY = Color(64, 64, 64)
+
+#: Pure black color (RGB: 0, 0, 0)
+BLACK = Color(0, 0, 0)
+
+#: Orange color (RGB: 255, 165, 0)
+ORANGE = Color(255, 165, 0)
+
+#: Purple color (RGB: 128, 0, 128)
+PURPLE = Color(128, 0, 128)
+
+#: Brown color (RGB: 165, 42, 42)
+BROWN = Color(165, 42, 42)
+
+#: Pink color (RGB: 255, 192, 203)
+PINK = Color(255, 192, 203)
+
+#: Violet color (RGB: 238, 130, 238)
+VIOLET = Color(238, 130, 238)
+
+#: Indigo color (RGB: 75, 0, 130)
+INDIGO = Color(75, 0, 130)
+
+#: Teal color (RGB: 0, 128, 128)
+TEAL = Color(0, 128, 128)
+
+#: Gold color (RGB: 255, 215, 0)
+GOLD = Color(255, 215, 0)
+
+#: Coral color (RGB: 255, 127, 80)
+CORAL = Color(255, 127, 80)
+
+#: Turquoise color (RGB: 64, 224, 208)
+TURQUOISE = Color(64, 224, 208)
+
+#: Rose color (RGB: 255, 0, 128)
+ROSE = Color(255, 0, 128)
+
+#: Heliotrope color (RGB: 223, 115, 255)
+HELIOTROPE = Color(223, 115, 255)
+
+#: Azure color (RGB: 0, 127, 255)
+AZURE = Color(0, 127, 255)
+
+#: Sea Green color (RGB: 46, 139, 87)
+SEA_GREEN = Color(46, 139, 87)
+
+#: Spring Green color (RGB: 0, 250, 154)
+SPRING_GREEN = Color(0, 250, 154)
+
+#: Olive color (RGB: 107, 142, 35)
+OLIVE = Color(107, 142, 35)
+
+#: Yellow Green color (RGB: 154, 205, 50)
+YELLOW_GREEN = Color(154, 205, 50)
+
+#: Army Green color (RGB: 75, 83, 32)
+ARMY_GREEN = Color(75, 83, 32)
 
